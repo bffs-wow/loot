dbfcbec90817ca95699e9114fff65663df3c89db
Sean Arthur <seanmarthur@gmail.com>
2020-08-12 08:13:57 -0400

    Randomize background img on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'synergy-loot';
+export class AppComponent implements OnInit {
+  backgrounds = [1, 2];
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    const randomImage = this.backgrounds[Math.floor(Math.random() * this.backgrounds.length)]
+    this.renderer.setStyle(document.body, 'background-image', `url(./assets/images/background-${randomImage}.jpg)`);
+
+  }
 }
