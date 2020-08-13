462b75352c215be183efcd33a203a9fa5aeb25df
Sean Arthur <seanmarthur@gmail.com>
2020-08-13 10:00:42 -0400

    Add more backgrounds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,20 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  backgrounds = [1, 2];
+  backgrounds = [1, 2, 3, 4, 5];
   constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
-    const randomImage = this.backgrounds[Math.floor(Math.random() * this.backgrounds.length)]
-    this.renderer.setStyle(document.body, 'background-image', `url(./assets/images/background-${randomImage}.jpg)`);
-
+    const randomImage = this.backgrounds[
+      Math.floor(Math.random() * this.backgrounds.length)
+    ];
+    this.renderer.setStyle(
+      document.body,
+      'background-image',
+      `url(./assets/images/background-${randomImage}.jpg)`
+    );
   }
 }
