d3a321fac724ee8c61b59c52e4a0a281cbfafc5b
Sean Arthur <seanmarthur@gmail.com>
2020-08-13 12:44:07 -0400

    Add indicators for competition between the selected raider and others on their loot list.

--- src/app/loot-list/loot-list.component.ts
+++ src/app/loot-list/loot-list.component.ts
@@ -1,26 +1,83 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Raider } from './models/raider.model';
-import {  LootReceipt } from './models/loot.model';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { LootReceipt, LootGroup, EligibleLoot } from './models/loot.model';
+import { faCheck, faStopwatch20, faMinus, faStopwatch, faCalendar, faCalendarCheck, faAward, faClock } from '@fortawesome/free-solid-svg-icons';
 import { Ranking } from './models/ranking.model';
+import { LootListFacadeService } from './loot-list.facade';
+import { map, tap, take, first } from 'rxjs/operators';
+import { forkJoin, combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-loot-list',
   templateUrl: './loot-list.component.html',
   styleUrls: ['./loot-list.component.scss'],
 })
-export class LootListComponent implements OnInit {
+export class LootListComponent implements OnInit, OnChanges {
   @Input() raider: Raider;
   @Input() noRouteLink = false;
+  competition: { [itemName: string]: EligibleLoot[] } = {};
   faCheck = faCheck;
-  constructor() {}
+  constructor(private lootListFacade: LootListFacadeService) {}
 
   ngOnInit(): void {}
 
+  ngOnChanges(changes) {
+    if (changes.raider) {
+      combineLatest(
+        this.raider.rankings.map((ranking) =>
+          this.getCompetition(ranking).pipe(
+            map((group) => ({ group, ranking }))
+          )
+        )
+      )
+        .pipe(
+          first(),
+          tap((groups) => {
+            for (const group of groups) {
+              this.competition[group.ranking.itemName] = group.group;
+            }
+          })
+        )
+        .subscribe();
+    }
+  }
+
   receivedLoot(ranking: Ranking): LootReceipt {
     if (this.raider && ranking) {
-      return this.raider.receivedLoot.find((l) => l.sheetName === ranking.itemName);
+      return this.raider.receivedLoot.find(
+        (l) => l.sheetName === ranking.itemName
+      );
     }
     return null;
+  }
+
+  getCompetition(ranking: Ranking) {
+    return this.lootListFacade.getRankedLootGroups(ranking.loot.name).pipe(
+      map((groups) =>
+        groups.filter(
+          (grp) => grp.points >= ranking.ranking + this.raider.attendancePoints
+        )
+      ),
+      map((groups) =>
+        groups.map((grp) => {
+          grp.rankings = grp.rankings.filter(
+            (r) => r.raiderName !== this.raider.name
+          );
+          return grp;
+        }).filter(grp => grp.rankings.length > 0).map(grp => grp.rankings).reduce((acc, cur) => {
+          return [...cur, ...acc]
+        }, [])
+      )
+    );
+  }
+
+  getCompetitionIcon(place: number) {
+    if(place ===0) {
+      return faAward;
+    } else if (place <= 3) {
+      return faClock;
+    } else {
+      return faMinus;
+    }
   }
 }
