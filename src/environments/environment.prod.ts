b1ce7312734bf5e99ac38ef966b2280b4bef2a49
Sean Arthur <seanmarthur@gmail.com>
2023-01-18 23:08:45 -0500

    phase 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  cache_bust: '12',
+  cache_bust: '13',
   logsGuildUrl:
     'https://classic.warcraftlogs.com/guild/rankings/532692/latest/',
   logsCharacterBaseUrl:
     'https://classic.warcraftlogs.com/character/us/faerlina/',
   wishlistLength: 50,
   wishlistOffspecCutoff: 33,
-  forgiveness: 1.5,
+  forgiveness: 1,
   itemsPerSlotRule: 3,
   tmbBaseUrl: 'https://thatsmybis.com/8752/best-friends/',
   currentRaids: [
