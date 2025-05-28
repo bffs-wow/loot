fbb61fa5a2cdc610a391635679e1b2058855707e
Sean Arthur <seanmarthur@gmail.com>
2025-05-28 17:11:58 -0400

    Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,34 +12,27 @@ import { LootListComponent } from './loot-list/loot-list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ItemPageComponent } from './item-page/item-page.component';
 import { RaiderPageComponent } from './raider-page/raider-page.component';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { RosterPageComponent } from './roster-page/roster-page.component';
 import { SharedComponentsModule } from './shared-components/shared-components.module';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    LootListComponent,
-    ItemPageComponent,
-    RaiderPageComponent,
-    RosterPageComponent,
-    AdminPageComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    NgSelectModule,
-    FormsModule,
-    ReactiveFormsModule,
-    LayoutsModule,
-    FontAwesomeModule,
-    SweetAlert2Module.forRoot(),
-    SharedComponentsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        LootListComponent,
+        ItemPageComponent,
+        RaiderPageComponent,
+        RosterPageComponent,
+        AdminPageComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        NgSelectModule,
+        FormsModule,
+        ReactiveFormsModule,
+        LayoutsModule,
+        FontAwesomeModule,
+        SharedComponentsModule], providers: [provideHttpClient(withInterceptorsFromDi())]
 })
 export class AppModule { }
